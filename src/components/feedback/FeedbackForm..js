2edf448119f229ecf513e050604014f55458c1e4
Abiral <abiralkc21@gmail.com>
2022-04-08 21:19:50 +0545

    added rating

--- src/components/feedback/FeedbackForm..js
+++ src/components/feedback/FeedbackForm..js
@@ -1,30 +1,50 @@
 import { useState } from 'react';
 import Card from '../common/Card';
 import Button from './../common/Button';
+import RatingSelect from './RatingSelect';
 
-const FeedbackForm = () => {
+const FeedbackForm = ({ addFeedback }) => {
   const [text, setText] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [message, setMessage] = useState('');
+  const [rating, setRating] = useState(null);
 
   function handleInputText(e) {
-    console.log(e);
     setText(e.target.value);
+
+    if (e.target.value === '' || e.target.value.trim().length <= 5) {
+      setDisabled(true);
+      setMessage('Text should be greater than 5 character');
+    } else {
+      setDisabled(false);
+      setMessage('');
+    }
   }
 
   return (
     <Card>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addFeedback({ text, rating });
+          setText('');
+          setRating(null);
+        }}
+      >
         <h3>Please type your feedback below.</h3>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className='input-group'>
           <input
             type='text'
             placeholder='Write your review'
             onChange={handleInputText}
+            value={text}
           />
-          <Button type='submit' version='primary'>
+          <Button type='submit' version='primary' isDisabled={disabled}>
             Send
           </Button>
         </div>
-        {text}
+        {message && <p style={{ color: 'red' }}>{message}</p>}
       </form>
     </Card>
   );
